2105d2dd6023f5a2397bc8704815878720e174ad
Akbuy <86617077+Akbuy@users.noreply.github.com>
2023-01-21 11:36:05 +0500

    Добавьте файлы проекта.

---
+++ Source/ShootThemUp/ShootThemUpGameModeBase.cpp
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ShootThemUpGameModeBase.h"
+#include "Player/STUBaseCharacter.h"
+#include "Player/STUPlayerController.h"
+
+AShootThemUpGameModeBase::AShootThemUpGameModeBase()
+{
+    DefaultPawnClass = ASTUBaseCharacter::StaticClass();
+    PlayerControllerClass = ASTUPlayerController::StaticClass();
+}
